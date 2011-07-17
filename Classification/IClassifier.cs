c4270c23c1e4b864d88a572d943f83dc7cb29d44
Dmitrij Koniajev <dimchansky@gmail.com>
2011-07-17 23:23:52 +0300

    Added classifier interface.

---
+++ Classification/IClassifier.cs
@@ -0,0 +1,11 @@
+﻿namespace MachineLearning.Classification
+{
+    using System;
+
+    interface IClassifier<out TCategory, in TAttribute>
+        where TCategory : IEquatable<TCategory>
+        where TAttribute : IEquatable<TAttribute>
+    {
+        TCategory Classify(TAttribute[] attributes);
+    }
+}
