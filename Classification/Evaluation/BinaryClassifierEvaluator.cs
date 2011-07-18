5c80d802e271768452b1e0b69f8ee3684a8c40fa
Dmitrij Koniajev <dimchansky@gmail.com>
2011-07-18 19:59:32 +0300

    added new class class BinaryClassifierEvaluator<TAttribute>. Changed Train method signature in ISupervisedClassifier interface.

---
+++ Classification/Evaluation/BinaryClassifierEvaluator.cs
@@ -0,0 +1,66 @@
+﻿namespace MachineLearning.Classification.Evaluation
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BinaryClassifierEvaluator<TAttribute>
+        where TAttribute : IEquatable<TAttribute>
+    {
+        private readonly ConfusionMatrix confusionMatrix = new ConfusionMatrix();
+        private readonly IClassifier<bool, TAttribute> classifier;
+
+        public ConfusionMatrix ConfusionMatrix
+        {
+            get
+            {
+                return this.confusionMatrix;
+            }
+        }
+
+        public BinaryClassifierEvaluator(IClassifier<bool, TAttribute> classifier)
+        {
+            if (classifier == null)
+            {
+                throw new ArgumentNullException("classifier");
+            }
+            this.classifier = classifier;
+        }
+
+        public void UpdateEvaluation(IEnumerable<TrainingSample<bool, TAttribute>> testSamples)
+        {
+            foreach (var testSample in testSamples)
+            {
+                UpdateEvaluation(testSample);
+            }
+        }
+
+        public void UpdateEvaluation(TrainingSample<bool, TAttribute> testSample)
+        {
+            var predictedCategory = classifier.Classify(testSample.Attributes);
+            var actualCategory = testSample.Category;
+
+            if (predictedCategory)
+            {   // positive predicted
+                if (actualCategory)
+                {
+                    confusionMatrix.TruePositivesCount += testSample.Count;
+                }
+                else
+                {
+                    confusionMatrix.FalsePositivesCount += testSample.Count;
+                }
+            }
+            else
+            {   // negative predicted
+                if (actualCategory)
+                {
+                    confusionMatrix.FalseNegativesCount += testSample.Count;
+                }
+                else
+                {
+                    confusionMatrix.TrueNegativesCount += testSample.Count;
+                }                
+            }
+        }
+    }
+}
--- Classification/IDataSet.cs
+++ Classification/IDataSet.cs
@@ -3,10 +3,10 @@
     using System;
     using System.Collections.Generic;
 
-    public interface ISupervisedClassifier<TCategory, TAttribute> : IClassifier<TCategory, TAttribute>
+    public interface IDataSet<TCategory, TAttribute> : IClassifier<TCategory, TAttribute>
         where TCategory : IEquatable<TCategory>
         where TAttribute : IEquatable<TAttribute>
     {
-        void Train(IDataSet<TCategory, TAttribute> trainingDataSet);
+        IEnumerable<TrainingSample<TCategory, TAttribute>> GetData();
     }
 }
