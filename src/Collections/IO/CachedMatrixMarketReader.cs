5a121966adfaf627ba08847e77c52f30200b6fe6
Dmitrij Koniajev <dimchansky@gmail.com>
2011-08-10 19:10:03 +0300

    Added new class CachedMatrixMarketReader. Not implemented yet.

---
+++ src/Collections/IO/CachedMatrixMarketReader.cs
@@ -0,0 +1,73 @@
+﻿namespace MachineLearning.Collections.IO
+{
+    using System;
+    using System.Collections.Generic;
+
+    using MachineLearning.Collections.Array;
+
+    public class CachedMatrixMarketReader<T> : ISparseMatrixReader<T>, IDisposable
+        where T : struct, IEquatable<T>
+    {
+        #region Fields and Properties
+
+        private readonly ISparseMatrixReader<T> reader;
+
+        #endregion
+
+        #region Constructors
+
+        public CachedMatrixMarketReader(ISparseMatrixReader<T> reader)
+        {
+            if (reader == null)
+            {
+                throw new ArgumentNullException("reader");
+            }
+
+            this.reader = reader;
+        }
+
+        #endregion
+
+        #region Implementation of ISparseMatrixReader<T>
+
+        public int RowsCount
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int ColumnsCount
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public long ElementsCount
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerable<SparseVector<T>> ReadRows()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Implementation of IDisposable
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Tests/Collections.Tests/IO/CachedMatrixMarketReaderTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Collections.Tests.IO
+{
+    using System;
+
+    using MachineLearning.Collections.IO;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CachedMatrixMarketReaderTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorThrowsArgumentNullExceptionForNullReaderArgument()
+        {
+            // arrange
+            
+            // act
+            var cachedReader = new CachedMatrixMarketReader<double>(null);
+
+            // assert
+            Assert.Fail();
+        }
+    }
+}
