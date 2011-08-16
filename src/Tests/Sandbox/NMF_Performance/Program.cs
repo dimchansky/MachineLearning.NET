08df2a32b2a4979687a65a255a9f8bacb50d5068
Dmitrij Koniajev <dimchansky@gmail.com>
2011-08-17 00:57:23 +0300

    Added NMF_Performance project in Test\Sandbox to play with NMF.

---
+++ src/Tests/Sandbox/NMF_Performance/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace NMF_Performance
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    using MachineLearning.Collections.IO;
+    using MachineLearning.NonnegativeMatrixFactorization;
+
+    using TestHelpers;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string matrixFileName = @"c:\temp\matrix.txt";
+            const int rows = 10000;
+            const int columns = 1000;
+            const int avgNonZeroElementsInRow = 3;
+
+            const int featuresCount = 10;
+            const int iterationsCount = 30;
+
+            Console.WriteLine("Generating matrix {0}x{1} with avg. {2} non-zero elements ({3}%)...", rows, columns, (long)avgNonZeroElementsInRow * rows, (double)avgNonZeroElementsInRow * 100.0 / columns);
+            using (var file = new FileStream(matrixFileName, FileMode.Create))
+            using (var mmwriter = new MatrixMarketWriter<double>(file))
+            {
+                var vectors = SparseVectorHelper.GenerateSparceVectors(rows, columns, (double)(columns - avgNonZeroElementsInRow) / columns, true);
+                mmwriter.Write(vectors);
+            }
+
+            Console.WriteLine("Matrix factorization (features=" + featuresCount + ", iterations=" + iterationsCount + ")...");
+            using (var file = new FileStream(matrixFileName, FileMode.Open))
+            using (var mmreader = new MatrixMarketReader<double>(file))
+            using (var cachedReader = new CachedMatrixMarketReader<double>(mmreader))
+            {
+                var nmf = new NMF(cachedReader);
+
+                // act
+                var sw = Stopwatch.StartNew();
+                using (var factorization = nmf.Factorize(featuresCount, iterationsCount))
+                {
+                    sw.Stop();                    
+                    Console.WriteLine("Factorization time: " + sw.ElapsedMilliseconds + " ms.");
+
+                    Console.WriteLine("Euclidean distance calculation...");
+                    sw = Stopwatch.StartNew();
+                    var euclideanDistance = nmf.GetEuclideanDistance(factorization);
+                    sw.Stop();
+                    Console.WriteLine("Euclidean distance calculation time: " + sw.ElapsedMilliseconds + " ms.");                   
+                    Console.WriteLine("Factorization Euclidean distance: " + euclideanDistance);                    
+                }
+            }
+        }
+    }
+}
